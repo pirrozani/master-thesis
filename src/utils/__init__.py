32db4f2698945d01a0313645e10c53810cba67b7
pirrozani <pirrozani@outlook.com>
2026-07-05 23:18:15 +0300

    refactor(evaluation): move metric functions into src/utils/metrics

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,19 @@
 from src.utils.files import save_file
+from src.utils.metrics import (
+    class_metrics,
+    levenshtein_distance,
+    levenshtein_similarity,
+    normalize_text,
+    similarity_bucket,
+    token_f1,
+)
 
-__all__ = ['save_file']
+__all__ = [
+    'save_file',
+    'class_metrics',
+    'levenshtein_distance',
+    'levenshtein_similarity',
+    'normalize_text',
+    'similarity_bucket',
+    'token_f1',
+]

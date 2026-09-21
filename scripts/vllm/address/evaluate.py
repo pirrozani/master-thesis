dc09586db49fc7de433258d377d180ee863751aa
pirrozani <pirrozani@outlook.com>
2026-09-21 12:19:46 +0300

    chore(evaluation): remove obsolete evaluation on docker

--- scripts/vllm/address/evaluate.py
+++
@@ -1,7 +0,0 @@
-"""Score the vLLM-served address adapter on a dataset split."""
-
-from src.vllm.evaluation import main
-from src.vllm.tasks import ADDRESS
-
-if __name__ == '__main__':
-    main(ADDRESS)
--- scripts/vllm/extraction/evaluate.py
+++
@@ -1,7 +0,0 @@
-"""Score the vLLM-served entity-name extraction adapter on a dataset split."""
-
-from src.vllm.evaluation import main
-from src.vllm.tasks import EXTRACTION
-
-if __name__ == '__main__':
-    main(EXTRACTION)
